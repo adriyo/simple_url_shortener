9c0eb55919867f109102306f1f44c1bccea9440b
Adri Driyo Utomo <adriyoutomo@gmail.com>
2024-09-01 21:27:38 +0700

    first commit

---
+++ shortener/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from .models import UrlMapping
+
+
+admin.site.register(UrlMapping)
---
+++ shortener/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+
+class User(models.Model):
+    user_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=150)
+
+
+class UrlMapping(models.Model):
+    short_url = models.CharField(max_length=10)
+    long_url = models.TextField()
+    created_at = models.DateTimeField()
+    expired_at = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    click_count = models.IntegerField()
