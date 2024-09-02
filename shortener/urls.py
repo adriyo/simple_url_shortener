09e41f2bedae9e13b9e64af061748c54edf81159
Adri Driyo Utomo <adriyoutomo@gmail.com>
2024-09-02 22:49:28 +0700

    added basic form result

--- shortener/urls.py
+++ shortener/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "shortener"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("result/", views.result, name="result"),
 ]
--- shortener/views.py
+++ shortener/views.py
@@ -1,5 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 
 
 def index(request):
-    return HttpResponse("Hello world news")
+    context = {}
+    return render(request, "shortener/index.html", context)
+
+
+def result(request):
+    try:
+        url = request.POST["url"]
+        alias = request.POST["alias"]
+        context = {"url": url, "alias": alias}
+        return render(request, "shortener/result.html", context)
+    except KeyError:
+        return HttpResponseRedirect(reverse("shortener:index"))
