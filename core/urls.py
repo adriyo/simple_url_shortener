3f7c099d37e2cfe7018ec72d6a10fae2c9ac807d
Adri Driyo Utomo <adriyoutomo@gmail.com>
2025-01-29 19:14:37 +0700

    added registration page

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "core"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login", views.login, name="login")
+    path("login", views.login, name="login"),
+    path("register", views.register, name="register"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
+
 def index(request):
     return render(request, "index.html")
+
 
 def login(request):
     try:
@@ -17,3 +19,36 @@ def login(request):
         "error": "Email or password is invalid",
     }
     return render(request, "index.html", context)
+
+
+def register(request):
+    if request.method == "POST":
+        return submitRegistration(request)
+
+    return render(request, "register.html")
+
+
+def submitRegistration(request):
+    try:
+        email = request.POST["email"]
+        password = request.POST["password"]
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        context = {
+            "email": email,
+            "password": password,
+            "first_name": first_name,
+            "last_name": last_name,
+            "username": get_default_username(email),
+        }
+    except KeyError:
+        return redirect(reverse("core:register"))
+    print(context)
+    return redirect("core:index")
+
+
+def get_default_username(email):
+    import re
+
+    local_part = email.split("@")[0]
+    return re.sub(r"[^a-zA-Z0-9]", "", local_part)
